1ad4a360456bc0f7b642e4496342b91e85d32efb
LiRuikuan <liruikuan@outlook.com>
2020-12-05 20:19:06 +0800

    day5-part2-sort

--- day5/main.go
+++ day5/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -33,7 +34,36 @@ func part1() {
 }
 
 func part2() {
-
+	list := sort.IntSlice{}
+	for _, seat := range strings.Split(input, "\n") {
+		rl, rr := 0, 127
+		for _, v := range seat[0:7] {
+			if rune(v) == 'F' {
+				rr = (rl + rr) / 2
+			} else {
+				rl = (rl+rr)/2 + 1
+			}
+		}
+		cl, cr := 0, 7
+		for _, v := range seat[7:] {
+			if rune(v) == 'L' {
+				cr = (cl + cr) / 2
+			} else {
+				cl = (cl+cr)/2 + 1
+			}
+		}
+		seatID := rl*8 + cl
+		list = append(list, seatID)
+	}
+	list.Sort()
+	seatID := 0
+	for i := 0; i < len(list)-1; i++ {
+		if list[i+1] != list[i]+1 {
+			seatID = list[i] + 1
+			break
+		}
+	}
+	fmt.Println(seatID)
 }
 
 func main() {
