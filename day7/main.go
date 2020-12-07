1200d2f75cc1a29f3bff881d2574473f12cf7218
LiRuikuan <liruikuan@outlook.com>
2020-12-07 17:04:19 +0800

    part2

--- day7/main.go
+++ day7/main.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
+type containeeBag struct {
+	count int
+	bag   *colorBag
+}
 type colorBag struct {
 	color      string
 	containers []*colorBag
+	containees []*containeeBag
+}
+
+func constructGraph() *colorBag {
+	bagMap := map[string]*colorBag{}
+	for _, line := range strings.Split(input, "\n") {
+		idx := strings.Index(line, " bags")
+		parentColor := line[:idx]
+		if bagMap[parentColor] == nil {
+			bagMap[parentColor] = &colorBag{color: parentColor}
+		}
+		container := bagMap[parentColor]
+		line = line[strings.Index(line, "contain")+7:]
+		for _, childLine := range strings.Split(line, ",") {
+			childLine = strings.Trim(childLine, " ")
+			if childLine != "" {
+				idx = strings.Index(childLine, " ")
+				count, _ := strconv.Atoi(childLine[:idx])
+				childLine = childLine[idx+1:]
+				idx = strings.Index(childLine, " bag")
+				childColor := childLine[:idx]
+				if bagMap[childColor] == nil {
+					bagMap[childColor] = &colorBag{color: childColor}
+				}
+				containee := bagMap[childColor]
+				containee.containers = append(containee.containers, container)
+
+				eeBag := &containeeBag{count: count, bag: containee}
+				container.containees = append(container.containees, eeBag)
+			}
+		}
+	}
+	return bagMap["shiny gold"]
 }
 
 func traverse(node *colorBag, containerColors map[string]bool) {
@@ -21,38 +59,27 @@ func traverse(node *colorBag, containerColors map[string]bool) {
 }
 
 func part1() {
-	bagMap := map[string]*colorBag{}
-	for _, line := range strings.Split(input, "\n") {
-
-		idx := strings.Index(line, " bags")
-		parentColor := line[:idx]
-		if bagMap[parentColor] == nil {
-			bagMap[parentColor] = &colorBag{color: parentColor}
-		}
-		container := bagMap[parentColor]
-		line = line[strings.Index(line, "contain")+7:]
-		for _, childLine := range strings.Split(line, ",") {
-			childLine = strings.Trim(childLine, " ")
-			if childLine != "" {
-				idx = strings.Index(childLine, " ")
-				childLine = childLine[idx+1:]
-				idx = strings.Index(childLine, " bag")
-				childColor := childLine[:idx]
-				if bagMap[childColor] == nil {
-					bagMap[childColor] = &colorBag{color: childColor}
-				}
-				containee := bagMap[childColor]
-				containee.containers = append(containee.containers, container)
-			}
-		}
-	}
-
+	shinyGoldBag := constructGraph()
 	containerColors := map[string]bool{}
-	traverse(bagMap["shiny gold"], containerColors)
+	traverse(shinyGoldBag, containerColors)
 	fmt.Println(len(containerColors) - 1)
+}
+
+func traverseCount(bag *colorBag) int {
+	count := 1
+	for _, ee := range bag.containees {
+		count += ee.count * traverseCount(ee.bag)
+	}
+	return count
+}
+
+func part2() {
+	shinyGoldBag := constructGraph()
+	count := traverseCount(shinyGoldBag) - 1 // -1 to exclude shiny gold bag itself
+	fmt.Println(count)
 }
 
 func main() {
 	part1()
-
+	part2()
 }
