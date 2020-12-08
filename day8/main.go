8d9d1498b895a0f0d64f2edc23bde945cb69243b
LiRuikuan <liruikuan@outlook.com>
2020-12-08 16:28:27 +0800

    refactor

--- day8/main.go
+++ day8/main.go
@@ -8,31 +8,8 @@ import (
 
 func part1() {
 	insts := strings.Split(input, "\n")
-	execed := make([]bool, len(insts))
-	accumulator := 0
-	idx := 0
-	for {
-		if execed[idx] {
-			break
-		}
-		execed[idx] = true
-		pair := strings.Split(insts[idx], " ")
-		inst := pair[0]
-		switch inst {
-		case "acc":
-			arg, _ := strconv.Atoi(pair[1])
-			accumulator += arg
-			break
-		case "jmp":
-			arg, _ := strconv.Atoi(pair[1])
-			idx += arg
-			continue
-		case "nop":
-			break
-		}
-		idx++
-	}
-	fmt.Println(accumulator)
+	acc, _ := exec(insts)
+	fmt.Println(acc)
 }
 
 func exec(insts []string) (accumulator int, terminate bool) {
