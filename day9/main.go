9bde8bb3c7a89c8caf924dc71e81bf5501ac510e
LiRuikuan <liruikuan@outlook.com>
2020-12-09 15:23:28 +0800

    part2

--- day9/main.go
+++ day9/main.go
@@ -17,14 +17,13 @@ func isValid(ring *[25]int, val int) bool {
 	return false
 }
 
-func part1() {
+func part1() int {
 	ring := [25]int{}
 	for idx, val := range strings.Split(input, "\n") {
 		ival, _ := strconv.Atoi(val)
 		if idx >= 25 {
 			if !isValid(&ring, ival) {
-				fmt.Println(ival)
-				return
+				return ival
 			}
 			ring[idx%25] = ival
 
@@ -32,8 +31,44 @@ func part1() {
 			ring[idx] = ival
 		}
 	}
+	panic("no such number.")
+}
+
+func part2() {
+	nums := strings.Split(input, "\n")
+	slice := []int{}
+	sum := 0
+	r := 0
+	target := part1()
+	for r < len(nums) {
+		if sum == target {
+			min, max := slice[0], slice[0]
+			for _, v := range slice {
+				if min > v {
+					min = v
+				}
+				if max < v {
+					max = v
+				}
+			}
+			fmt.Println(min + max)
+			return
+		}
+		for sum < target {
+			rVal, _ := strconv.Atoi(nums[r])
+			sum += rVal
+			slice = append(slice, rVal)
+			r++
+		}
+		for sum > target {
+			sum -= slice[0]
+			slice = slice[1:]
+		}
+	}
+	panic("no such range.")
 }
 
 func main() {
-	part1()
+	fmt.Println(part1())
+	part2()
 }
