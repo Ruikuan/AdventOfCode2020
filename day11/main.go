b2d6a07b059dead6b306d38bb3bbf987b4b6469f
LiRuikuan <liruikuan@outlook.com>
2020-12-11 18:50:19 +0800

    part2

--- day11/main.go
+++ day11/main.go
@@ -87,12 +87,12 @@ func printState(state [][]rune) {
 	fmt.Println()
 }
 
-func part1() int {
+func runPart(fn func([][]rune) [][]rune) int {
 	state := getInput()
 	eq := false
 	for !eq {
 		//printState(state)
-		nextState := applyRule(state)
+		nextState := fn(state)
 		eq = isEqual(state, nextState)
 		state = nextState
 	}
@@ -107,6 +107,79 @@ func part1() int {
 	return occ
 }
 
+func part1() int {
+	return runPart(applyRule)
+}
+
+func applyRule2(state [][]rune) [][]rune {
+	nextState := make([][]rune, len(state))
+	changes := [8][2]int{
+		{-1, -1},
+		{-1, 0},
+		{-1, 1},
+		{0, -1},
+		{0, 1},
+		{1, -1},
+		{1, 0},
+		{1, 1},
+	}
+	for i := 0; i < len(state); i++ {
+		l := len(state[i])
+		nextState[i] = make([]rune, l)
+		for j := 0; j < l; j++ {
+			if state[i][j] == '.' {
+				nextState[i][j] = '.'
+			} else if state[i][j] == 'L' {
+				nextState[i][j] = '#'
+			l1:
+				for _, ch := range changes {
+					for scale := 1; scale < l || scale < len(state); scale++ {
+						x, y := i+scale*ch[0], j+scale*ch[1]
+						if x < 0 || y < 0 || x >= len(state) || y >= l {
+							break
+						}
+						if state[x][y] == 'L' {
+							break
+						} else if state[x][y] == '#' {
+							nextState[i][j] = 'L'
+							break l1
+						}
+					}
+				}
+			} else if state[i][j] == '#' {
+				occAdj := 0
+				nextState[i][j] = '#'
+			l2:
+				for _, ch := range changes {
+					for scale := 1; scale < l || scale < len(state); scale++ {
+						x, y := i+scale*ch[0], j+scale*ch[1]
+						if x < 0 || y < 0 || x >= len(state) || y >= l {
+							break
+						}
+						if state[x][y] == 'L' {
+							break
+						}
+						if state[x][y] == '#' {
+							occAdj++
+							if occAdj >= 5 {
+								nextState[i][j] = 'L'
+								break l2
+							}
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+	return nextState
+}
+
+func part2() int {
+	return runPart(applyRule2)
+}
+
 func main() {
 	fmt.Println(part1())
+	fmt.Println(part2())
 }
