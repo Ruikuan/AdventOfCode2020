f52a9316d1832ab49754f55fc551f36f7544f578
LiRuikuan <liruikuan@outlook.com>
2020-12-12 15:06:35 +0800

    part2

--- day12/main.go
+++ day12/main.go
@@ -54,6 +54,56 @@ func part1() int {
 	return int(math.Abs(float64(pos[0]))) + int(math.Abs(float64(pos[1])))
 }
 
+func rRotate90(x, y int) (int, int) {
+	return y, -x
+}
+func lRotate90(x, y int) (int, int) {
+	return -y, x
+}
+
+func part2() int {
+	waypoint := [2]int{10, 1}
+	pos := [2]int{0, 0}
+	for _, ins := range strings.Split(input, "\n") {
+		act := ins[0]
+		val, _ := strconv.Atoi(ins[1:])
+		switch act {
+		case 'N':
+			waypoint[1] = waypoint[1] + val
+			break
+		case 'S':
+			waypoint[1] = waypoint[1] - val
+			break
+		case 'E':
+			waypoint[0] = waypoint[0] + val
+			break
+		case 'W':
+			waypoint[0] = waypoint[0] - val
+			break
+		case 'F':
+			pos[0] = pos[0] + waypoint[0]*val
+			pos[1] = pos[1] + waypoint[1]*val
+			break
+		case 'L':
+			val = val % 360
+			val = val / 90
+			for i := 0; i < val; i++ {
+				waypoint[0], waypoint[1] = lRotate90(waypoint[0], waypoint[1])
+			}
+			break
+		case 'R':
+			val = val % 360
+			val = val / 90
+			for i := 0; i < val; i++ {
+				waypoint[0], waypoint[1] = rRotate90(waypoint[0], waypoint[1])
+			}
+			break
+		}
+	}
+	return int(math.Abs(float64(pos[0]))) + int(math.Abs(float64(pos[1])))
+}
+
 func main() {
 	fmt.Println(part1())
+	fmt.Println(part2())
 }
